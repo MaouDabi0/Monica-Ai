66ba6d67a3c29fba6d5a084c508cb5fcb648313e
MaouDabi <erkipandial@gmail.com>
2025-11-06 06:16:11 +0700

    Update Monica-Ai
    update 1.1.0

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const startBot = async () => {
     const { state, saveCreds } = await useMultiFileAuthState('./connect/session')
     xp = makeWASocket({
       auth: state,
+      version: [2e0, 3e3, 1.025150051e9],
       printQRInTerminal: !1,
       syncFullHistory: !1,
       logger: logLevel,
@@ -49,10 +50,11 @@ const startBot = async () => {
 
     xp.ev.on('messages.upsert', async ({ messages }) => {
       for (let m of messages) {
+        m = cleanMsg(m)
         if (!m.message) continue
 
         m = replaceLid(m)
-        if (m.key.fromMe) cleanMsg(m)
+        log(m)
 
         const { id, group, sender, pushName, channel } = global.chat(m, botName),
               time = global.time.timeIndo('Asia/Jakarta', 'HH:mm'),
